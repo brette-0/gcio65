621f98fdf458741893cfff8c6bda4543a5e39f5a
Brette <98058652+brette-0@users.noreply.github.com>
2026-04-01 18:41:19 +0100

    Worked on assembly after spec change

--- gcio65.s
+++ gcio65.s
@@ -24,6 +24,8 @@
     .define concat(foo, bar) foo ## bar
     .define ld(r) ld ## r
     .define st(r) st ## r
+    .define de(i) de ## i
+    .define in(i) in ## i
 
     .ifndef IOPORT1
         .define IOPORT1 $4016
@@ -40,41 +42,93 @@
         )
     .endif
 
+    .macro __CWriteBit msg, s, r
+        .local d0, d1
+
+        .if s = 0
+            ld(r) #msg & 1
+            st(r) IOPORT1
+            bit   IOPORT1
+
+            .exitmacro
+        .endif
+
+        d0 = (sr(msg, s))       & 1
+        d1 = (sr(msg, (s + 1))) & 1
+
+        .if d0 <> d1
+            .if r = a
+                eor #$01
+            .else
+                .if d1
+                    in(r)
+                .else
+                    de(r)
+                .endif
+            .endif
+        .endif
+
+        st(r) IOPORT1
+        bit   IOPORT1
+    .endmacro
+
+    ; internal function to write paylods to the adaptor
+    .macro __CWritePayload msg, type, r
+        .if type = GCIO_BEHAVE
+            .if message > 8
+                .error "gcio65.payload message is too big!"
+            .endif
+            
+            .repeat 8, s
+                __CWriteBit msg, s, r
+            .endrepeat
+        .elseif type = GCIO_INMASK .or type = GCIO_INVERT
+            .if message > 64
+                .error "gcio65.payload message is too big!"
+            .endif
+
+            .repeat 64, s
+                __CWriteBit msg, s, r
+            .endrepeat
+        .endif
+    .endmacro
+
+    .macro __ChangeTask task, r
+        .if task > GCIO_RUMBLE
+            .error "gcio.__ChangeTask task is not a valid task!"
+        .endif
+        
+        .repeat task, _
+            ld(r) IOPORT1
+        .endrepeat
+    .endmacro
+
+    .macro __RestoreTask task, r
+        .repeat GCIO_RUMBLE + 1 - task, _
+            ld(r) IOPORT1
+        .endrepeat
+    .endmacro
+
+    .macro __WriteQWord qword
+        .local fetch, loop
+
+                ldy #0
+        loop:   ldx #8
+                lda pMask, y
+                sta $4016
+                lsr
+                dex
+                bne loop
+        fetch:  iny
+                cpy #8
+                bne loop
+    .endmacro
+
     ; waste [a,x,y]
     ; ptr   [invert]
-    ; size  [nInvert]
-    ; ptr   [temp]
-    .macro RTChangeInvert invert, nInvert, temp, i
-        .local loop, exit, enter
-
+    .macro RTChangeInvert invert, temp, i
         __SwitchTask GCIO_INVERT, a
-
-        ldx #0
-        lda #1
-        ldy #8
-        sec
-        bcs enter
-
-        sLoop:
-            sta IOPORT1
-            bcs check
-        cLoop:
-            bit IOPORT0
-        check:
-            dey
-            bne enter
-            ldy #8
-            dec temp
-            bmi exit
-            bne enter
-            ldy #2
-            inx
-        enter:
-            rol invert, x
-            bcc cLoop
-            bcs sLoop
-        exit:
-        
+        __WriteQWord invert
         __RestoreTask GCIO_INVERT, a
     .endmacro
 
@@ -105,11 +159,8 @@
         __RestoreTask GCIO_INVERT, _r
     .endmacro
 
-    ; in    [a]
-    ; waste [i]
+    ; waste [a,i]
     .macro RTChangeMask pMask, i
-        .local loop, exit, enter
-
         .if i = x
             _i = x
         .elseif i = y
@@ -119,36 +170,8 @@
         .endif
     
         __SwitchTask GCIO_INMASK, _i
-
-        .repeat 6, o
-            lda pMask + o
-
-            .if o = 5
-                ld(_i) #2
-            .else
-                ld(_i) #8
-            .endif
-
-            bne enter
-        
-            loop:
-                dex
-                beq exit
-
-            enter:
-                lsr
-                bcc r
-                bcs w
-
-                r:
-                    ld(r) IOPORT1
-                    bcc loop
-                w:
-                    st(r) IOPORT1
-                    bcs loop
-        .endrepeat
-        exit:
-            __RestoreTask GCIO_INMASK, _i
+        __WriteQWord pMask    
+        __RestoreTask GCIO_INMASK, _i
     .endmacro
 
     .macro CChangeMask mask, r
@@ -215,51 +238,6 @@
             bne loop
         
         __RestoreTask GCIO_BEHAVE, _i
-    .endmacro
-
-    ; internal function to write paylods to the adaptor
-    .macro __CWritePayload msg, type, r
-        .if type = GCIO_BEHAVE
-            .if message > 8
-                .error "gcio65.payload message is too big!"
-            .endif
-            
-            .repeat 8, s
-                .if (message >> s) & 1
-                    st(r) IOPORT1
-                .else
-                    ld(r) IOPORT1
-                .endif
-            .endrepeat
-        .elseif type = GCIO_INMASK
-            .if message > 61
-                .error "gcio65.payload message is too big!"
-            .endif
-
-            .repeat 64, s
-                .if sr(message, s) & 1
-                    st(r) IOPORT1
-                .else
-                    ld(r) IOPORT1
-                .endif
-            .endrepeat
-        .endif
-    .endmacro
-
-    .macro __ChangeTask task, r
-        .if task > GCIO_RUMBLE
-            .error "gcio.__ChangeTask task is not a valid task!"
-        .endif
-        
-        .repeat task, _
-            ld(r) IOPORT1
-        .endrepeat
-    .endmacro
-
-    .macro __RestoreTask task, r
-        .repeat GCIO_RUMBLE + 1 - task, _
-            ld(r) IOPORT1
-        .endrepeat
     .endmacro
 
     .export RTChangeInvert
